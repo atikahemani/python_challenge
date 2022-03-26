99d77333e461e5589a226a896cd77e4a411b9764
Atika Hemani <atikahemani@iMac.local>
2022-03-26 16:10:54 -0500

    hw changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,47 @@
 
+# reading in the budget_data.csv file
+
+#import packages needed to read csv file
+import os
+import csv
+print(os.path.abspath('.'))
+
+csvpath = os.path.join('', 'Resources', 'budget_data.csv')
+
+profit=[]
+monthly_changes=[]
+date=[]
+
+count=0
+total=0
+total_change_profit=0
+initial_profit=0
+
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+
+
+    for row in csvreader:
+        count+=1
+        date.append([0])
+
+        profit.append([1])
+        total+=int(row[1])
+
+        final_profit=int(row[1])
+       
+    for i in range(1,count):
+        monthly_changes.append(count[i]-count[i-1])
+
+        average=sum(monthly_changes)/len(monthly_changes)
+
+    print(monthly_changes)
+
+  
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,69 @@
 
+
+# reading in the budget_data.csv file
+
+#import packages needed to read csv file
+from calendar import c
+import os
+import csv
+print(os.path.abspath('.'))
+
+csvpath = os.path.join('', 'Resources', 'election_data.csv')
+
+
+
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    candidates = {}
+    total_votes=0
+    most_votes=0
+    votes=0
+
+
+
+    for row in csvreader:
+        candidate_name=row[2]
+        total_votes+=1
+
+        if candidate_name in candidates.keys():
+            candidates[candidate_name] +=1
+        else:
+            candidates[candidate_name]=1
+
+    for candidate_name in candidates:
+
+        percent_of_votes = (candidates[candidate_name])/(total_votes) * 100
+        #print(f"{candidate_name}: {int(percent_of_votes)}% {total_votes}")
+        
+        if candidates[candidate_name] > most_votes:
+            most_voted=candidate_name
+            most_votes=candidates[candidate_name]
+
+print(candidates)
+print("Election Results")
+print("-------------------------------")
+print(f"Total Votes: {total_votes}")
+print("-------------------------------")
+for candidate_name in candidates:
+    percent_of_votes = (candidates[candidate_name])/(total_votes) * 100
+    print(f"{candidate_name}: {int(percent_of_votes)}% {list(candidates.values())}")
+print("-------------------------------")
+print(f"Winner: {most_voted}")
+        
+#with open('election_results.txt', 'w') as text:
+    #text.write("Election Results\n")
+    #text.write("-------------------------------\n")
+    #text.write(f"Total Votes: {total_votes}\n")
+    #text.write("-------------------------------\n")
+    #for candidate_name in candidates:
+        #percent_of_votes = (candidates[candidate_name])/(total_votes) * 100
+        #text.write(f"{candidate_name}: {int(percent_of_votes)}% {total_votes}\n")
+   # text.write("-------------------------------\n")
+    #text.write(f"Winner: {most_voted}\n")
